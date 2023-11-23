909a93299b8a410699d8e688d338eecf5f2addb1
ArthurMenegoli <arthur.menegoli@gmail.com>
2023-11-22 22:40:31 -0300

    Fix identification of pending order

--- Model/Job.php
+++ Model/Job.php
@@ -12,14 +12,16 @@ class Cammino_Autocancel_Model_Job
         Mage::log('Horas atualmente configuradas para expiração: ' . $expires_at, null, 'autocancel.log');
         $fromDate = date('Y-m-d H:i:s', strtotime('-' . $expires_at . 'hour'));
         Mage::log('Pegar pedidos a partir de: ' . $expires_at, null, 'autocancel.log');
-        $selectedMethods = Mage::getStoreConfig('autocancel/general/payment_methods');
+        $selectedMethods = explode(',', Mage::getStoreConfig('autocancel/general/payment_methods'));
         foreach ($selectedMethods as $method) {
             Mage::log('Checando pedidos status pending e com o método: ' . $method, null, 'autocancel.log');
-            $paymentCollection =  Mage::getModel('sales/order_payment')->getCollection()->addFieldToFilter('method', $method)->addFieldToFilter('status', array('in' => array('pending')));
+            $paymentCollection =  Mage::getModel('sales/order_payment')->getCollection()->addFieldToFilter('method', $method);
             foreach ($paymentCollection as $payment) {
                 try {
                     $order =  Mage::getModel('sales/order')->load($payment->getId());
+                    Mage::log('Identificou a order: ' . $order->getId(), null, 'autocancel.log');
                     if (($order->getCreatedAt() < $fromDate) && ($order->getStatus() == 'pending')) {
+                        Mage::log('ORDER COM CREATED AT MENOR QUE O FROM DATE E STATUS PENDING! ' . $order->getId(), null, 'autocancel.log');
                         if ($order->canCancel()) {
                             $comment = "Pedido cancelado automaticamente por falta de pagamento antes do prazo de expiração.";
                             $order->addStatusHistoryComment($comment, false);
